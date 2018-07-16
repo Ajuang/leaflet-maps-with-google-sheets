6f9afb6db06d4bb887720b2e260620a40b592581
Ajuang <ajumodee@gmail.com>
2018-07-16 13:09:24 +0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1rvU0Zuk2I0MVMlocI5VJU9Wy1mt7Bg8-HluTnLg3nkI/edit#gid=0';
